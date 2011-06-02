e3ec626a31623911f45a544925e8392a4f44cf9d
Guilherme Reis Campos <guilhermekbsa@gmail.com>
2011-06-02 01:02:40 -0300

    adding the database file

---
+++ banco.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE agenda_telefonica;
+CREATE TABLE registros (
+  nome VARCHAR(200),
+  email VARCHAR(200) PRIMARY,
+  telefone_residencial VARCHAR(15),
+  telefone_celular VARCHAR(15)
+);
+
