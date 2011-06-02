be862d791bb9de186bf3d2347ee57e79bde73ff4
Guilherme Reis Campos <guilhermekbsa@gmail.com>
2011-06-02 01:03:51 -0300

    fixing database creator

--- banco.sql
+++ banco.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE agenda_telefonica;
 CREATE TABLE registros (
+  email VARCHAR(200) PRIMARY KEY,
   nome VARCHAR(200),
-  email VARCHAR(200) PRIMARY,
   telefone_residencial VARCHAR(15),
   telefone_celular VARCHAR(15)
 );
