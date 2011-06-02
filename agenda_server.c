6bfcc39ecfd8e5002e0862870c3f0b225941ea9d
Guilherme Reis Campos <guilhermekbsa@gmail.com>
2011-06-01 23:32:35 -0300

    connecting server to postgresql

--- agenda_server.c
+++ agenda_server.c
@@ -1,4 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "agenda.h"
+#include "libpq-fe.h"
 
 int *
 inicializar_1_svc(void *argp, struct svc_req *rqstp)
@@ -16,11 +19,46 @@ int *
 insere_1_svc(registro *argp, struct svc_req *rqstp)
 {
 	static int  result;
+  const char *conninfo;
+  PGconn     *conn;
+  PGresult   *res;
+  const char *insert_query = malloc(sizeof(registro));
+  sprintf(insert_query,"INSERT INTO registros VALUES('%s', '%s', '%s','%s');",*argp.nome,*argp.email,*argp.telefone_celular,*argp_telefone_residencial));
 
-	/*
-	 * insert server code here
-	 */
+  /*
+   * If the user supplies a parameter on the command line, use it as the
+   * conninfo string; otherwise default to setting dbname=postgres and using
+   * environment variables or defaults for all other connection parameters.
+   */
+  conninfo = "username = root dbname = agenda_telefonica";
+  /* Make a connection to the database */
+  conn = PQconnectdb(conninfo);
 
+  /* Check to see that the backend connection was successfully made */
+  if (PQstatus(conn) != CONNECTION_OK)
+  {
+      fprintf(stderr, "Connection to database failed: %s", PQerrorMessage(conn));
+      result = 2;  //banco nao esta ON.
+      return &result;
+  }
+
+  res = PQexec(conn, query);
+  if (PQresultStatus(res) != PGRES_COMMAND_OK)
+  {
+      fprintf(stderr, "INSERT command failed: %s", PQerrorMessage(conn));
+      result = 0;
+      return &result;
+  }
+
+  /*
+   * should PQclear PGresult whenever it is no longer needed to avoid memory
+   * leaks
+   */
+  PQclear(res);
+
+  /* close the connection to the database and cleanup */
+  PQfinish(conn);
+  result = 1;
 	return &result;
 }
 
